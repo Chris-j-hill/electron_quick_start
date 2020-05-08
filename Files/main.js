39cda3d5a454aa2558d795a443cc57f169d584a4
Chris <chris.j.hill94@gmail.com>
2020-05-08 21:15:14 +0100

    added initial config parsing

--- Files/main.js
+++ Files/main.js
@@ -3,23 +3,74 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
+let height_d = 600
+let width_d = 800
+let devs_enabled = false
+
+
+
+function print(content){
+  console.log(content)
+}
+
+
+// read config file for window parameters
+function read_config() {
+  let fs = require('fs')
+  let filename = 'config.txt'
+
+  let content = fs.readFileSync(filename, 'utf8', (err) => {console.log("read err")});
+
+  content = content.split("\n")
+
+  for (let i = 0; i < content.length; i++){
+    if (content[i].indexOf("#") !== 0) {
+      line = content[i].split(":")
+      if (line[0] === "window_size_x") {
+        line = line[1].split("\"")
+        line = line[1]
+        width_d = parseInt(line,10)
+      }
+      else if (line[0] === "window_size_y") {
+        line = line[1].split("\"")
+        line = line[1]
+        height_d = parseInt(line,10)
+      }
+      else if (line[0] === "enable_dev_options") {
+        line = line[1].split("\"")
+        line = line[1]
+        if (line.toLowerCase() === "true"){
+          devs_enabled = true
+        }
+      }
+
+    }
+  }
+}
+
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+
+    width: width_d,
+    height: height_d,
     webPreferences: {
       //preload: path.join(__dirname, 'preload.js')
       nodeIntegration: true
     }
   })
-
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  if (devs_enabled === true){
+   mainWindow.webContents.openDevTools()
+ }
 }
+
+  read_config()
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
--- quick_start.bat
+++ quick_start.bat
@@ -2,4 +2,4 @@ git clone https://github.com/electron/electron-quick-start
 copy Files\* electron-quick-start
 cd electron-quick-start
 npm install
-REM npm start
+del .git*
