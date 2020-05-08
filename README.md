b2cea70fe89786c7986e15e76ee8550da36dd8c5
Chris <chris.j.hill94@gmail.com>
2020-05-08 20:05:26 +0100

    copy in existing files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 electron_quick_start
+
+prerequisites
+
+intall git  https://git-scm.com/downloads
+install nodejs  https://nodejs.org/en/download/ 
---
+++ quick_start.bat
@@ -0,0 +1,5 @@
+git clone https://github.com/electron/electron-quick-start
+copy Files\* electron-quick-start
+cd electron-quick-start
+npm install
+REM npm start
